82c5e763b4f96728eabb0ac26cb0743d9f43afc0
Vladimir Venegas <vladimir@MacBook-Pro-de-Vladimir.local>
2020-12-05 17:22:51 -0300

    se agrega archivo nuevo que contiene la información de los robots en el plano

---
+++ src/camera_processor.py
@@ -0,0 +1,22 @@
+class CameraProcessor:
+
+    azul
+    rojo
+    verde
+    celeste
+    morado
+    amarillo
+
+    # Esta funcion debe retornar una tupla con el par de coordenadas (x,y)
+    # para un robot en particular.
+    # Los ids posibles son: azul, rojo, verde, celeste, morado y amarillo
+    def getRobotPosition(robotId):
+        return (0, 0)
+
+    # Esta función lee de forma continua como while(true),
+    # y almacena los datos en los campos definidos de cada robot
+    # Por ejemplo, va a guardar el par de cordenadas "x,y" 
+    # del robot azul en la propiedad "azul" como una tupla (30, 24)
+    def runProcess():
+
+    
